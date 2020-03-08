9e05eb23189968b1a2ae40b233333edecfd11189
kazuaki25nigo <island25style1982@gmail.com>
2020-03-08 21:07:04 +0900

    chat-spacemessage
    kannryou

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+# メッセージ送信機能の実装に必要なルーティングは、以下の通りです。
+# 投稿されたメッセージの一覧表示 & メッセージの入力ができる:index
+# メッセージの保存を行う:create
   end
 end
