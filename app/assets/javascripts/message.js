0117fe28c3d0d0a79cc3752e1377f50359347f85
kazuaki25nigo <island25style1982@gmail.com>
2020-03-22 17:13:51 +0900

    automatic-updating
    不要コメント削除確認お願いします。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -42,7 +42,6 @@ $(function(){
   
   // インクリメンタルサーチの記述。
   $('#new_message').on('submit', function(e){
-    // console.logを用いてイベント発火しているか確認
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -64,42 +63,34 @@ $(function(){
       alert('エラーが発生したためメッセージは送信できませんでした。');
     })
     .always(function(data){
-      $('.form__submit').prop('disabled', false);//ここで解除している
+      $('.form__submit').prop('disabled', false);
     })
   })
-
-  // 自動更新はここから。 
-
+  
+// 自動更新はここから。 
   var reloadMessages = function () {
-    // if (window.location.href.match(/\/groups\/\d+\/messages/)){//今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。
     var last_message_id = $('.message:last').data("message-id");
-    
-          // var group_id = $(".group").data("group-id");
-
-      $.ajax({ //ajax通信で以下のことを行う
-        url: "api/messages", //サーバを指定。今回はapi/message_controllerに処理を飛ばす
-        type: 'get', //メソッドを指定
-        dataType: 'json', //データはjson形式
-        data: {last_id: last_message_id} //飛ばすデータは先ほど取得したlast_message_id。またparamsとして渡すためlast_idとする。
+      $.ajax({ 
+        url: "api/messages", 
+        type: 'get', 
+        dataType: 'json', 
+        data: {last_id: last_message_id} 
       })
-      .done(function (messages) { //通信成功したら、controllerから受け取ったデータ（messages)を引数にとって以下のことを行う
+      .done(function (messages) { 
         if (messages.length !== 0) {
-        var insertHTML = '';//追加するHTMLの入れ物を作る
-        messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-          insertHTML += buildHTML(message); //メッセージが入ったHTMLを取得
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML += buildHTML(message); 
         })
-        $('.messages').append(insertHTML);//メッセージを追加
+        $('.messages').append(insertHTML);
         $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       }
     })
       .fail(function () {
-        alert('自動更新に失敗しました');//ダメだったらアラートを出す
+        alert('自動更新に失敗しました');
       });
-    
     };
-
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);//7000ミリ秒ごとにreloadMessagesという関数を実行し自動更新を行う。
+    setInterval(reloadMessages, 7000);
   };
-  
 });
